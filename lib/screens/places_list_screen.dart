4bfa2d511941e46976f217d1e20a6a703c9acd3f
kibriatuhin <tuinn78@gmail.com>
2022-05-16 16:45:23 +0600

    push in github

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Camera & Location"),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.add))
+        ],
+      ),
+      body: Center(
+       // child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
